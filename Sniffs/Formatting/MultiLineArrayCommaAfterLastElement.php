9150da40040ac8015f3160f03127deebbbebc130
Xaver Loppenstedt <xaver@loppenstedt.de>
2013-09-20 10:57:52 +0200

    added sniff for multi line array comma after last element

---
+++ Sniffs/Formatting/MultiLineArrayCommaAfterLastElement.php
@@ -0,0 +1,93 @@
+<?php
+
+/**
+ * This file is part of the Symfony2-coding-standard (phpcs standard)
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer-Symfony2
+ * @author   Symfony2-phpcs-authors <Symfony2-coding-standard@opensky.github.com>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @version  GIT: master
+ * @link     https://github.com/opensky/Symfony2-coding-standard
+ */
+
+/**
+ * Symfony2_Sniffs_Formatting_BlankLineBeforeReturnSniff.
+ *
+ * Throws errors if there's no blank line before return statements. Symfony
+ * coding standard specifies: "Add a blank line before return statements,
+ * unless the return is alone inside a statement-group (like an if statement);"
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer-Symfony2
+ * @author   Xaver Loppenstedt <xaver@loppenstedt.de>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @link     https://github.com/opensky/Symfony2-coding-standard
+ */
+class Symfony_Sniffs_Formatting_MultiLineArrayCommaAfterLastElement implements PHP_CodeSniffer_Sniff
+{
+    /**
+     * A list of tokenizers this sniff supports.
+     *
+     * @var array
+     */
+    public $supportedTokenizers = array('PHP');
+
+    /**
+     * Returns an array of tokens this test wants to listen for.
+     *
+     * @return array
+     */
+    public function register()
+    {
+        // T_OPEN_SHORT_ARRAY
+        // T_CLOSE_SHORT_ARRAY
+        return array(T_ARRAY);
+    }
+
+    /**
+     * Processes this test, when one of its tokens is encountered.
+     *
+     * @param PHP_CodeSniffer_File $phpcsFile All the tokens found in the document.
+     * @param int                  $stackPtr  The position of the current token in
+     *                                        the stack passed in $tokens.
+     *
+     * @return void
+     */
+    public function process(PHP_CodeSniffer_File $phpcsFile, $stackPtr)
+    {
+        $tokens = $phpcsFile->getTokens();
+        $line   = $tokens[$stackPtr]['line'];
+
+        $arrayStart = $tokens[$stackPtr]['parenthesis_opener'];
+        $arrayEnd   = $tokens[$arrayStart]['parenthesis_closer'];
+
+        if ($tokens[$arrayStart]['line'] === $tokens[$arrayEnd]['line']) {
+            // single line
+            return;
+        }
+
+        $lastContent = $phpcsFile->findPrevious(T_WHITESPACE, ($arrayEnd - 1), $arrayStart, true);
+        if ($tokens[$lastContent]['line'] !== ($tokens[$arrayEnd]['line'] -1 ) {
+            $phpcsFile->addError(
+                'Closing parenthesis of an array must be on a new line',
+                $arrayEnd,
+                'CloseBraceNewLine'
+            );
+        }
+
+        // find last Item
+        // Fail if no comma between last item and $arrayEnd
+        
+
+
+        //
+        // $phpcsFile->addError(
+            // 'missing comma after each array item in a multi-line array',
+            // $stackPtr
+        // );
+        return;
+    }
+}
---
+++ Tests/Formatting/MultiLineArrayCommaAfterLastElementUnitTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * This file is part of the Symfony2-coding-standard (phpcs standard)
+ *
+ * PHP version 5
+ *
+ * @category PHP
+ * @package  PHP_CodeSniffer-Symfony2
+ * @author   Symfony2-phpcs-authors <Symfony2-coding-standard@opensky.github.com>
+ * @license  http://spdx.org/licenses/MIT MIT License
+ * @version  GIT: master
+ * @link     https://github.com/opensky/Symfony2-coding-standard
+ */
+
+/**
+ * Unit test class for the MultiLineArrayCommaAfterLastElementTest sniff.
+ *
+ * A sniff unit test checks a .inc file for expected violations of a single
+ * coding standard. Expected errors and warnings are stored in this class.
+ *
+ * @category  PHP
+ * @package   PHP_CodeSniffer
+ * @author    Xaver Loppenstedt <xaver@loppenstedt.de>
+ * @copyright 2012 Xaver Loppenstedt, some rights reserved.
+ * @license   http://spdx.org/licenses/MIT MIT License
+ * @link      https://github.com/opensky/Symfony2-coding-standard
+ */
+class Symfony_Tests_Formatting_MultiLineArrayCommaAfterLastElementUnitTest extends AbstractSniffUnitTest
+{
+    /**
+     * Returns the lines where errors should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of errors that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getErrorList($testFile = '')
+    {
+        switch ($testFile) {
+        case 'MultiLineArrayCommaAfterLastElementUnitTest.1.inc':
+            return array();
+        // case 'MultiLineArrayCommaAfterLastElementUnitTest.2.inc':
+        // case 'MultiLineArrayCommaAfterLastElementUnitTest.3.inc':
+        //    return array(7 => 1);
+        }
+
+        return array();
+    }
+
+    /**
+     * Returns the lines where warnings should occur.
+     *
+     * The key of the array should represent the line number and the value
+     * should represent the number of warnings that should occur on that line.
+     *
+     * @return array(int => int)
+     */
+    public function getWarningList()
+    {
+        return array();
+    }
+}
